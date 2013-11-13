7fed327890e10c38d89df2c7913f906d76e515e7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-11-13 00:42:22 -0500

    journald: do not free space when disk space runs low
    Before, journald would remove journal files until both MaxUse= and
    KeepFree= settings would be satisfied. The first one depends (if set
    automatically) on the size of the file system and is constant.  But
    the second one depends on current use of the file system, and a spike
    in disk usage would cause journald to delete journal files, trying to
    reach usage which would leave 15% of the disk free. This behaviour is
    surprising for the user who doesn't expect his logs to be purged when
    disk usage goes above 85%, which on a large disk could be some
    gigabytes from being full. In addition attempting to keep 15% free
    provides an attack vector where filling the disk sufficiently disposes
    of almost all logs.
    
    Instead, obey KeepFree= only as a limit on adding additional files.
    When replacing old files with new, ignore KeepFree=. This means that
    if journal disk usage reached some high point that at some later point
    start to violate the KeepFree= constraint, journald will not add files
    to go above this point, but it will stay (slightly) below it. When
    journald is restarted, it forgets the previous maximum usage value,
    and sets the limit based on the current usage, so if disk remains to
    be filled, journald might use one journal-file-size less on each
    restart, if restarts happen just after rotation. This seems like a
    reasonable compromise between implementation complexity and robustness.
    
    (cherry picked from commit 348ced909724a1331b85d57aede80a102a00e428)

--- src/journal/test-journal-interleaving.c
+++ src/journal/test-journal-interleaving.c
@@ -194,7 +194,7 @@ static void test_skip(void (*setup)(void))
         if (arg_keep)
                 log_info("Not removing %s", t);
         else {
-                journal_directory_vacuum(".", 3000000, 0, 0, NULL);
+                journal_directory_vacuum(".", 3000000, 0, NULL);
 
                 assert_se(rm_rf_dangerous(t, false, true, false) >= 0);
         }
@@ -279,7 +279,7 @@ static void test_sequence_numbers(void) {
         if (arg_keep)
                 log_info("Not removing %s", t);
         else {
-                journal_directory_vacuum(".", 3000000, 0, 0, NULL);
+                journal_directory_vacuum(".", 3000000, 0, NULL);
 
                 assert_se(rm_rf_dangerous(t, false, true, false) >= 0);
         }
