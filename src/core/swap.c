InitWare/InitWare

commit f84b1b1ff9b1261cc9ee339fe0e5447f1573f807

core: do not segfault if swap activity happens when /proc/swaps is not open
In https://bugzilla.redhat.com/show_bug.cgi?id=969795 systemd crashes
in swap_dispatch_reload called from manager_loop becuase m->proc_swaps
is NULL. It can legitimately be NULL if something went wrong when
initially enumerating swap devices when starting the manager. This
is probably a sign of significant trouble, but let's do our best
to recover.

src/core/swap.c
@@ -1068,13 +1068,39 @@ static int swap_load_proc_swaps(Manager *m, bool set_flags) {
         return r;
 }
 
+static int open_proc_swaps(Manager *m) {
+        if (!m->proc_swaps) {
+                struct epoll_event ev = {
+                        .events = EPOLLPRI,
+                        .data.ptr = &m->swap_watch,
+                };
+
+                m->proc_swaps = fopen("/proc/swaps", "re");
+                if (!m->proc_swaps)
+                        return (errno == ENOENT) ? 0 : -errno;
+
+                m->swap_watch.type = WATCH_SWAP;
+                m->swap_watch.fd = fileno(m->proc_swaps);
+
+                if (epoll_ctl(m->epoll_fd, EPOLL_CTL_ADD, m->swap_watch.fd, &ev) < 0)
+                        return -errno;
+        }
+
+        return 0;
+}
+
 int swap_dispatch_reload(Manager *m) {
         /* This function should go as soon as the kernel properly notifies us */
+        int r;
 
         if (_likely_(!m->request_reload))
                 return 0;
 
         m->request_reload = false;
+
+        r = open_proc_swaps(m);
+        if (r < 0)
+                return r;
 
         return swap_fd_event(m, EPOLLPRI);
 }
@@ -1225,22 +1251,9 @@ static int swap_enumerate(Manager *m) {
         int r;
         assert(m);
 
-        if (!m->proc_swaps) {
-                struct epoll_event ev = {
-                        .events = EPOLLPRI,
-                        .data.ptr = &m->swap_watch,
-                };
-
-                m->proc_swaps = fopen("/proc/swaps", "re");
-                if (!m->proc_swaps)
-                        return (errno == ENOENT) ? 0 : -errno;
-
-                m->swap_watch.type = WATCH_SWAP;
-                m->swap_watch.fd = fileno(m->proc_swaps);
-
-                if (epoll_ctl(m->epoll_fd, EPOLL_CTL_ADD, m->swap_watch.fd, &ev) < 0)
-                        return -errno;
-        }
+        r = open_proc_swaps(m);
+        if (r < 0)
+                return r;
 
         r = swap_load_proc_swaps(m, false);
         if (r < 0)

commit 817a2d0c98ca035e2a906c04a4e2509103c583b1

core: do not segfault if /proc/swaps cannot be opened
The refactoring in f84b1b1ff9b1261 ('core: do not segfault if swap
activity happens when /proc/swaps is not open') caused
swap_dispatch_reload and swap_enumerate to continue even if fopen()
failed with ENOENT.

This should instead be modified to return from swap_dispatch_reload and
swap_enumerate, rather than continuing to load the list of swaps when
m->proc_swaps is NULL.

https://bugzilla.redhat.com/show_bug.cgi?id=1069393

src/core/swap.c
@@ -1074,7 +1074,7 @@ static int open_proc_swaps(Manager *m) {
 
                 m->proc_swaps = fopen("/proc/swaps", "re");
                 if (!m->proc_swaps)
-                        return (errno == ENOENT) ? 0 : -errno;
+                        return -errno;
 
                 m->swap_watch.type = WATCH_SWAP;
                 m->swap_watch.fd = fileno(m->proc_swaps);
@@ -1097,7 +1097,7 @@ int swap_dispatch_reload(Manager *m) {
 
         r = open_proc_swaps(m);
         if (r < 0)
-                return r;
+                return (r == -ENOENT) ? 0 : r;
 
         return swap_fd_event(m, EPOLLPRI);
 }
@@ -1250,7 +1250,7 @@ static int swap_enumerate(Manager *m) {
 
         r = open_proc_swaps(m);
         if (r < 0)
-                return r;
+                return (r == -ENOENT) ? 0 : r;
 
         r = swap_load_proc_swaps(m, false);
         if (r < 0)