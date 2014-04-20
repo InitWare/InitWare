systemd/systemd

reduce the amount of messages logged to /dev/kmsg when "debug" is specified

src/core/main.c
@@ -412,18 +412,13 @@ static int parse_proc_cmdline_item(const char *key, const char *value) {
                 }
 
         } else if (streq(key, "quiet") && !value) {
-
                 if (arg_show_status == _SHOW_STATUS_UNSET)
                         arg_show_status = SHOW_STATUS_AUTO;
 
         } else if (streq(key, "debug") && !value) {
-
-                /* Log to kmsg, the journal socket will fill up before the
-                 * journal is started and tools running during that time
-                 * will block with every log message for for 60 seconds,
-                 * before they give up. */
                 log_set_max_level(LOG_DEBUG);
-                log_set_target(detect_container(NULL) > 0 ? LOG_TARGET_CONSOLE : LOG_TARGET_KMSG);
+                if (detect_container(NULL) > 0)
+                        log_set_target(LOG_TARGET_CONSOLE);
 
         } else if (!in_initrd() && !value) {
                 unsigned i;

InitWare/InitWare

(cherry picked from commit b2103dccb354de3f38c49c14ccb637bdf665e40f)
Conflicts:
	src/core/main.c
(cherry picked from commit c025499ceaf503d52f07a93fa8244f94fe6d6a73)

src/core/main.c
@@ -411,12 +411,9 @@ static int parse_proc_cmdline_word(const char *word) {
         } else if (streq(word, "quiet"))
                 arg_show_status = false;
         else if (streq(word, "debug")) {
-                /* Log to kmsg, the journal socket will fill up before the
-                 * journal is started and tools running during that time
-                 * will block with every log message for for 60 seconds,
-                 * before they give up. */
                 log_set_max_level(LOG_DEBUG);
-                log_set_target(LOG_TARGET_KMSG);
+                if (detect_container(NULL) > 0)
+                        log_set_target(LOG_TARGET_KMSG);
         } else if (!in_initrd()) {
                 unsigned i;
 