8038e07f24ced861a2c008e70c57ccafbff91e3c
Cristian Rodríguez <crrodriguez@opensuse.org>
2014-05-29 14:17:37 -0400

    tty-ask-password-agent: Do tell what directory we failed to open
    (cherry picked from commit 267b3e41df5a2181f2911433539f81de2fa1511a)
    (cherry picked from commit 54fcd1a651e89f4dd901dbecabf212f732006423)

--- src/tty-ask-password-agent/tty-ask-password-agent.c
+++ src/tty-ask-password-agent/tty-ask-password-agent.c
@@ -503,7 +503,7 @@ static int show_passwords(void) {
                 if (errno == ENOENT)
                         return 0;
 
-                log_error("opendir(): %m");
+                log_error("opendir(/run/systemd/ask-password): %m");
                 return -errno;
         }
 
