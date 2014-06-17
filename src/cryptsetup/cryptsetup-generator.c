4ca245757e771aa5650d568dfca7899051d3ac08
Lennart Poettering <lennart@poettering.net>
2014-06-18 00:07:56 +0200

    cryptsetup: introduce new cryptsetup-pre.traget unit so that services can make sure they are started before and stopped after any LUKS setup
    https://bugzilla.redhat.com/show_bug.cgi?id=1097938
    (cherry picked from commit 9542239eaf481decca6c254dffb3f2b4e716545e)
    (cherry picked from commit 137cb035d2facdf2a70f0c915d6c018b6d900933)

--- src/cryptsetup/cryptsetup-generator.c
+++ src/cryptsetup/cryptsetup-generator.c
@@ -119,7 +119,7 @@ static int create_disk(
                 "DefaultDependencies=no\n"
                 "BindsTo=dev-mapper-%i.device\n"
                 "IgnoreOnIsolate=true\n"
-                "After=systemd-readahead-collect.service systemd-readahead-replay.service\n",
+                "After=systemd-readahead-collect.service systemd-readahead-replay.service cryptsetup-pre.target\n",
                 f);
 
         if (!nofail)
